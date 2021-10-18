5da3a93db76c264d3bd515ee6804f1878da8aa7d
Sergey Morozov <morozov.serg901@gmail.com>
2021-10-19 02:39:20 +0300

    Added description for arithmetic operators

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include <vector>
 
 #include "boost/spirit/include/qi.hpp"
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/spirit/include/phoenix.hpp>
 #include <boost/variant.hpp>
 
 using namespace boost::spirit;
+namespace phx   = boost::phoenix;
 
 enum BinaryOperatorID {
     OR,
@@ -36,7 +39,6 @@ enum Types {
 
 typedef boost::variant<
     int,
-    bool,
     std::string
 > Literal;
 
@@ -50,16 +52,13 @@ struct FunctionCall;
 struct BinaryOperator;
 struct UnaryOperator;
 
-typedef boost::variant<
-    ID,
-    Literal,
-    FunctionCall
-> Value;
 
 typedef boost::variant<
 boost::recursive_wrapper<UnaryOperator>,
 boost::recursive_wrapper<BinaryOperator>,
-boost::recursive_wrapper<Value>
+    ID,
+    Literal,
+    FunctionCall
 > Expression;
 
 struct ExpressionList {
@@ -76,6 +75,9 @@ BOOST_FUSION_ADAPT_STRUCT(FunctionCall, (ID, name), (ExpressionList, args))
 struct UnaryOperator {
     UnaryOperatorID type;
     Expression right;
+
+    UnaryOperator() = default;
+    UnaryOperator(UnaryOperatorID type, Expression right) : type(type), right(right) {}
 };
 BOOST_FUSION_ADAPT_STRUCT(UnaryOperator, (UnaryOperatorID, type), (Expression, right))
 
@@ -83,6 +85,9 @@ struct BinaryOperator {
     BinaryOperatorID type;
     Expression left;
     Expression right;
+
+    BinaryOperator() = default;
+    BinaryOperator(Expression left, BinaryOperatorID type , Expression right) : type(type), left(left), right(right) {} 
 };
 BOOST_FUSION_ADAPT_STRUCT(BinaryOperator, (UnaryOperatorID, type), (Expression, left), (Expression, right))
 
@@ -98,9 +103,9 @@ typedef boost::variant<
 > IfStatement;
 
 struct ReturnStatement {
-    Value return_value;
+    Expression return_value;
 };
-BOOST_FUSION_ADAPT_STRUCT(ReturnStatement, (Value, return_value))
+BOOST_FUSION_ADAPT_STRUCT(ReturnStatement, (Expression, return_value))
 
 typedef boost::variant<
     Expression,
@@ -156,9 +161,9 @@ struct FuncDeclaration {
 BOOST_FUSION_ADAPT_STRUCT(FuncDeclaration, (ID, name), (TypeParametersList, args), (Statement, body))
 
 struct Language {
-    std::vector<FuncDeclaration> sequence;
+    std::vector<Expression> sequence;
 };
-BOOST_FUSION_ADAPT_STRUCT(Language, (std::vector<FuncDeclaration>, sequence))
+BOOST_FUSION_ADAPT_STRUCT(Language, (std::vector<Expression>, sequence))
 
 
 template<typename Iterator, typename Skipper>
@@ -169,6 +174,8 @@ struct Grammar: qi::grammar<Iterator, Skipper, Language()> {
             ("||", BinaryOperatorID::OR);
         AND_OP.add
             ("&&", BinaryOperatorID::AND);
+        NOT_OP.add
+            ("!", UnaryOperatorID::NOT);
         COMPARE_OP.add
             ("==", BinaryOperatorID::EQ)
             ("/=", BinaryOperatorID::NE)
@@ -182,18 +189,61 @@ struct Grammar: qi::grammar<Iterator, Skipper, Language()> {
         MULT_OP.add
             ("*", BinaryOperatorID::MULT)
             ("/", BinaryOperatorID::DIV);
+        UNARY_MINUS_OP.add
+            ("-", UnaryOperatorID::UNARY_MINUS);
         POW_OP.add
             ("^", BinaryOperatorID::POW);
 
+        OR_level = AND_level [ _val = _1 ] >> -(OR_OP >> OR_level) [ _val = phx::construct<BinaryOperator>(_val, _1, _2) ];
+        AND_level = NOT_level [ _val = _1 ] >> -(AND_OP >> AND_level) [ _val = phx::construct<BinaryOperator>(_val, _1, _2) ];
+        
+        NOT_level = COMPARE_level [ _val = _1 ] | (NOT_OP >> COMPARE_level) [ _val = phx::construct<UnaryOperator>(_1, _2) ];
+        
+        COMPARE_level = ADD_level [ _val = _1 ] >> -(COMPARE_OP >> ADD_level) [ _val = phx::construct<BinaryOperator>(_val, _1, _2) ];
+        
+        // Not left-associative!
+        ADD_level = MULT_level [ _val = _1 ] >> -(ADD_OP >> MULT_level) [ _val = phx::construct<BinaryOperator>(_val, _1, _2) ];
+        MULT_level = UNARY_MINUS_level [ _val = _1 ] >> -(MULT_OP >> UNARY_MINUS_level) [ _val = phx::construct<BinaryOperator>(_val, _1, _2) ];
+        //
+        
+        UNARY_MINUS_level = POW_level [_val = _1] | (UNARY_MINUS_OP >> POW_level) [ _val = phx::construct<UnaryOperator>(_1, _2) ];
+        
+        POW_level = value [_val = _1] >> -(POW_OP >> value) [ _val = phx::construct<BinaryOperator>(_val, _1, _2) ];
+
+
+        literal = qi::int_ | string_literal | bool_literal;
+        string_literal = qi::lexeme['"' >> *(ascii::char_ - '"') >> '"'];
+        
+        value = id | literal/* | func_call*/;
+        id = qi::lexeme[ascii::char_("a-z") >> *ascii::char_("0-9a-zA-Z")];
+
     }
+    
     qi::symbols<char, BinaryOperatorID> OR_OP,
                                         AND_OP,
                                         COMPARE_OP, 
                                         ADD_OP,
                                         MULT_OP,
                                         POW_OP;
+    qi::symbols<char, UnaryOperatorID>  UNARY_MINUS_OP,
+                                        NOT_OP;
     
+    qi::rule<Iterator, Skipper, Expression()> OR_level, AND_level, COMPARE_level, ADD_level, MULT_level, POW_level;
+    qi::rule<Iterator, Skipper, Expression()> UNARY_MINUS_level, NOT_level;
+
     qi::rule<Iterator, Skipper, Language()> start;
+    qi::rule<Iterator, Skipper, Expression()> expression;
+    qi::rule<Iterator, Skipper, Expression()>  value;
+    
+    qi::rule<Iterator, Skipper, ID()> id;
+
+    qi::rule<Iterator, Skipper, Literal()> literal;
+
+    qi::rule<Iterator, Skipper, Statement()> statement;
+
+    qi::rule<Iterator, Skipper, std::string()> string_literal;
+    qi::rule<Iterator, Skipper, std::string()> bool_literal;
+
 };
 
 
